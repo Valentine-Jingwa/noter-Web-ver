c41da48fbc984d39155b30cc7f5ce0a21a47907a
Valentine-Jingwa <119522550+Valentine-Jingwa@users.noreply.github.com>
2023-12-09 15:52:06 -0700

    Hover Changes

--- app/components/Navbar/NavigationBar.js
+++ app/components/Navbar/NavigationBar.js
@@ -1,5 +1,5 @@
 'use client';
-import {React, useState} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { BellIcon, InboxIcon, UserCircleIcon, DotsHorizontalIcon, HomeIcon, MapIcon, UserGroupIcon, FireIcon} from '@heroicons/react/solid';
@@ -7,13 +7,25 @@ import { BellIcon, InboxIcon, UserCircleIcon, DotsHorizontalIcon, HomeIcon, MapI
 
 
 export default function NavigationBar() {
-    // Drop down for settings
     const [showDropdown, setShowDropdown] = useState(false);
-    const toggleDropdown = () => setShowDropdown(!showDropdown);
-
-    // Drop down for notifications
     const [showNotification, setShowNotification] = useState(false);
-    const NotificationDropdown = () => setShowNotification(!showNotification);
+
+    const dropdownRef = useRef(null);
+    const notificationRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setShowDropdown(false);
+            }
+            if (notificationRef.current && !notificationRef.current.contains(event.target)) {
+                setShowNotification(false);
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => document.removeEventListener('mousedown', handleClickOutside);
+    }, [dropdownRef, notificationRef]);
 
     return (
         <>
@@ -27,43 +39,39 @@ export default function NavigationBar() {
                 </div>
                 <div className="hidden md:flex space-x-12">
 
-                    <Link href="/home" className="hover:text-gray-300 flex flex-col items-center space-x-2">
+                    <Link href="/home" className="hover:text-blue-400 flex flex-col items-center space-x-2">
                         <HomeIcon className="h-6 w-6" />
                         Home
                     </Link>
-                    <Link href="/about" className="hover:text-gray-300 flex flex-col items-center space-x-2">
+                    <Link href="/about" className="hover:text-blue-400 flex flex-col items-center space-x-2">
                         <UserGroupIcon className="h-6 w-6" />
                         Community</Link>
-                    <Link href="/services" className="hover:text-gray-300 flex flex-col items-center space-x-2">
+                    <Link href="/services" className="hover:text-blue-400 flex flex-col items-center space-x-2">
                         <MapIcon className="h-6 w-6" />
                         Near By activities</Link>
-                    <Link href="/contact" className="hover:text-gray-300 flex flex-col items-center space-x-2">
+                    <Link href="/contact" className="hover:text-blue-400 flex flex-col items-center space-x-2">
                         <FireIcon className="h-6 w-6" />
                         Create Activity</Link>
                 </div>
 
                 <div className="flex items-center space-x-10">
-                       <BellIcon className="h-6 w-6" onClick={NotificationDropdown}/> 
-                       {showNotification && (
-                        
-                        <div className="absolute right-10 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-20">
-                            <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Settings</a>
-                            {/* Add other menu items here */}
-                        </div>
-                    )}
-                    
-                <div className="relative">
-
-                
-                <DotsHorizontalIcon className="h-6 w-6 cursor-pointer" onClick={toggleDropdown} />
-                {showDropdown && (
-                    <div className="absolute right-0 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-20">
-                        <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Settings</a>
-                        {/* Add other menu items here */}
+                    <div ref={notificationRef}>
+                        <BellIcon className="h-6 w-6 cursor-pointer hover:text-blue-400" onClick={() => setShowNotification(!showNotification)} />
+                        {showNotification && (
+                            <div className="absolute right-20 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-20 h-[250px]">
+                                {/* Notification items */}
+                            </div>
+                        )}
                     </div>
-                )}
-            </div>
-                    
+
+                    <div ref={dropdownRef}>
+                        <DotsHorizontalIcon className="h-6 w-6 cursor-pointer hover:text-blue-400" onClick={() => setShowDropdown(!showDropdown)} />
+                        {showDropdown && (
+                            <div className="absolute right-2 mt-2 py-2 w-48 bg-white rounded-md shadow-xl z-20 h-[250px]">
+                                {/* Dropdown items */}
+                            </div>
+                        )}
+                    </div>
                 </div>
             </div>
         </>
